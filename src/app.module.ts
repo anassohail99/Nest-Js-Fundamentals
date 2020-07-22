044ecd63ba4e0b168d5122c4429edf2a166bcb99
anassohail99 <anas.sohailabs1@gmail.com>
2020-07-22 12:16:53 +0500

    Refactored

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
+import { Products } from './products/products.model';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsController } from './items/items.controller';
+// import { ItemsController } from './products/Products.controller';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ItemsController],
+  imports: [ProductsModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
